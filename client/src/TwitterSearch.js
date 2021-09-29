81359336a2b1f8e739bbae3f82154af853ea363a
Kevin W <kevinwestnyc@gmail.com>
2021-09-29 11:27:06 -0400

    server working

--- client/src/TwitterSearch.js
+++ client/src/TwitterSearch.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AccountSearchBar from './components/AccountSearchBar';
+import axios from 'axios';
 
 export default function TwitterSearch() {
+    
+    const[twitterData, setTwitterData] = useState([])
     
     function handleSearch(e, query) {
         e.preventDefault();
         setCurrentPageUrl(`https://api.twitter.com/2/tweets/search/recent?query=${query}&tweet.fields=public_metrics&expansions=author_id&user.fields=profile_image_url,verified`);
       }
+
+    useEffect(() => {
+        getData()
+    },[])  
+
+    async function getData() {
+        await axios.get('/api/search')
+        .then(response => setTwitterData(response.data))
+    }
+
+    // let itemsCards = <div>
+    //   {twitterData.map((x, i) =>
+    //     <TweetCard key={i} data={x} />
+    //   )}
+    //   </div>
     
+    
+    let loading = <div>
+      <p className="flow-text">Loading...</p>
+     </div>
+    
+    console.log(twitterData)
+
     return (
         <div className="container" >
             <h1 className="page-title">Search</h1>
@@ -16,15 +41,16 @@ export default function TwitterSearch() {
                     <div className="row">
                         {/* <p>Search by name, content or hashtag</p> */}
                         <AccountSearchBar handleSearch={handleSearch} />
-                    </div>
-                    <br />
-                    <div className="row">
-                        Search Results
-                    </div>
-                    
+                    </div>                 
                 </div>
                 <div className="col-9">
                     <h2>Results</h2>
+                    <div>
+                    {
+                        twitterData.length > 0 ? itemsCards : loading
+                    }
+
+                    </div>
                 </div>
             </div>
         
--- server.js
+++ server.js
@@ -4,49 +4,25 @@ const PORT = process.env.PORT || 3000;
 const axios = require('axios');
 const config = require("dotenv").config()
 const token = process.env.BEARER_TOKEN;
-
 const app = express();
-
-
-
-
-
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
 
-console.log("token: " + token)
 
-
+//const endpointUrl = "https://api.twitter.com/2/tweets/search/recent?query=${req.query.text}&tweet.fields=public_metrics&expansions=author_id&user.fields=profile_image_url,verified";
 const endpointUrl = "https://api.twitter.com/2/tweets/search/recent?query=49ers&tweet.fields=public_metrics&expansions=author_id&user.fields=profile_image_url,verified";
 
-async function getRequest() {
 
-    const res = await axios.get(endpointUrl, {
-        headers: {Authorization: `Bearer ${token}`}    
-    })
-
-    if (res.body) {
-        console.log(res.body)
-        return res.body;
-    } else {
-        throw new Error('Unsuccessful request');
-    }
-}
-
-(async () => {
-
-    try {
-        // Make request
-        const response = await getRequest();
-        console.dir(response, {
-            depth: null
-        });
-
-    } catch (e) {
-        console.log(e);
-        process.exit(-1);
-    }
-    process.exit();
-})();
+app.get("/api/search", async (req, res) => {
+    await axios
+      .get(endpointUrl, {
+        headers: { Authorization: ` Bearer ${token}`,}
+        }
+      )
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((error) => console.log(error));
+  });
