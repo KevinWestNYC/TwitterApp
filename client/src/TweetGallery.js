203ad5d3b813816bcb0156d249b3172bc64118c9
Kevin W <kevinwestnyc@gmail.com>
2021-10-01 10:20:56 -0400

    gallery page server/card setup

--- client/src/TweetGallery.js
+++ client/src/TweetGallery.js
@@ -1,31 +1,106 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import tweet1 from './images/tweet1.jpg'
 import tweet2 from './images/tweet2.jpg'
 import tweet3 from './images/tweet3.jpg'
 import ninersSched from './images/49ersSched.jpg'
+import axios from 'axios';
 import TweetCard from './components/TweetCard';
 // import LiveTweetList from './components/LiveTweetList'
 
 
 export default function TweetGallery() {
+
+    const [barrowsData, setBarrowsData] = useState([])
+    const [dogFeelingsData, setDogFeelingsData] = useState([])
+    const [factRetrieverData, setFactRetrieverData] = useState([])
+    const [conanData, setConanData] = useState([])
+
+    useEffect(() => {
+        getBarrowsData()
+        getDogFeelingsData()
+        getFactRetrieverData()
+        getConanData()
+    },[]) 
+    
+    async function getBarrowsData() {
+        await axios.get('/api/barrows')
+        .then(response => setBarrowsData(response.data))
+        
+    }
+
+    async function getDogFeelingsData() {
+        await axios.get('/api/dogFeelings')
+        .then(response => setDogFeelingsData(response.data))
+        
+    }
+
+    async function getFactRetrieverData() {
+        await axios.get('/api/factRetriever')
+        .then(response => setFactRetrieverData(response.data))
+        
+    }
+
+    async function getConanData() {
+        await axios.get('/api/conan')
+        .then(response => setConanData(response.data))
+        
+    }
+
+    let barrowsCards = <div>
+      {barrowsData.map((x, i) =>
+        <TweetCard key={i} data={x} />
+      )}
+      </div>
+
+    let dogFeelingsCards = <div>
+    {dogFeelingsData.map((x, i) =>
+    <TweetCard key={i} data={x} />
+    )}
+    </div>
+
+    let factRetrieverCards = <div>
+    {factRetrieverData.map((x, i) =>
+    <TweetCard key={i} data={x} />
+    )}
+    </div>
+
+    let conanCards = <div>
+    {conanData.map((x, i) =>
+    <TweetCard key={i} data={x} />
+    )}
+    </div>
+
+    let loading = <div>
+    <p className="flow-text">Loading...</p>
+    </div>
+
     return (
         <div className="container-fluid">
             <h1 className="page-title">Live</h1>
             <div className="row">
-                <div id="tweet-column" className="tweet-column col-2">
+                <div id="tweet-column" className="tweet-column col-3">
                     <h2>Tweets</h2>
+                    {
+                        barrowsData.length > 0 ? barrowsCards : loading
+                    }
                 </div>
-                <div id="tweet-column" className="tweet-column col-2">
+                <div id="tweet-column" className="tweet-column col-3">
                     <h2>Tweets</h2>
+                    {
+                        dogFeelingsData.length > 0 ? dogFeelingsCards : loading
+                    }
                 </div>
-                <div id="tweet-column" className="tweet-column col-2">
+                <div id="tweet-column" className="tweet-column col-3">
                     <h2>Tweets</h2>
+                    {
+                        factRetrieverData.length > 0 ? factRetrieverCards : loading
+                    }
                 </div>
-                <div id="tweet-column" className="tweet-column col-2">
+                <div id="tweet-column" className="tweet-column col-3">
                     <h2>Tweets</h2>
-                </div>
-                <div id="tweet-column" className="tweet-column col-2">
-                    <h2>Tweets</h2>
+                    {
+                        conanData.length > 0 ? conanCards : loading
+                    }
                 </div>
                 
             </div>
--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
 //const endpointUrl = "https://api.twitter.com/2/tweets/search/recent?query=${req.query.text}&tweet.fields=public_metrics&expansions=author_id&user.fields=profile_image_url,verified";
 const endpointUrl = "https://api.twitter.com/2/tweets/search/recent?query=49ers&tweet.fields=public_metrics,created_at&expansions=author_id&user.fields=profile_image_url,verified";
 //to search for specific user :'from%3Amattbarrows'
+const barrowsEndpoint = "https://api.twitter.com/2/users/35246396/tweets?max_results=5&expansions=author_id&tweet.fields=public_metrics,created_at&user.fields=profile_image_url,verified"
+const dogFeelingsEndpoint = "https://api.twitter.com/2/users/846137120209190912/tweets?max_results=5&expansions=author_id&tweet.fields=public_metrics,created_at&user.fields=profile_image_url,verified"
+const factRetrieverEndpoint = "https://api.twitter.com/2/users/55637356/tweets?max_results=5&expansions=author_id&tweet.fields=public_metrics,created_at&user.fields=profile_image_url,verified"
+const conanEndpoint = "https://api.twitter.com/2/users/115485051/tweets?max_results=5&expansions=author_id&tweet.fields=public_metrics,created_at&user.fields=profile_image_url,verified"
+
 
 app.get("/api/search", async (req, res) => {
   const data = [];  
@@ -25,3 +30,47 @@ app.get("/api/search", async (req, res) => {
       })
       .catch((error) => console.log(error));
   });
+
+  app.get("/api/barrows", async (req, res) => {
+    const data = [];  
+    await axios
+        .get(barrowsEndpoint, {headers: { Authorization: ` Bearer ${token}`,}})
+        .then((response) => {
+          data.push(response.data);
+          res.send(data);
+        })
+        .catch((error) => console.log(error));
+    });
+    
+    app.get("/api/dogFeelings", async (req, res) => {
+      const data = [];  
+      await axios
+          .get(dogFeelingsEndpoint, {headers: { Authorization: ` Bearer ${token}`,}})
+          .then((response) => {
+            data.push(response.data);
+            res.send(data);
+          })
+          .catch((error) => console.log(error));
+      });
+
+      app.get("/api/factRetriever", async (req, res) => {
+        const data = [];  
+        await axios
+            .get(factRetrieverEndpoint, {headers: { Authorization: ` Bearer ${token}`,}})
+            .then((response) => {
+              data.push(response.data);
+              res.send(data);
+            })
+            .catch((error) => console.log(error));
+        });
+
+        app.get("/api/conan", async (req, res) => {
+          const data = [];  
+          await axios
+              .get(conanEndpoint, {headers: { Authorization: ` Bearer ${token}`,}})
+              .then((response) => {
+                data.push(response.data);
+                res.send(data);
+              })
+              .catch((error) => console.log(error));
+          });
