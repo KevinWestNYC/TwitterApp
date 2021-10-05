34cc27b0d1ace015237f15e48a9474fa5b41a6b9
Kevin W <kevinwestnyc@gmail.com>
2021-10-05 19:25:30 -0400

    MVP update

--- client/src/Home.js
+++ client/src/Home.js
@@ -8,24 +8,10 @@ export default function Home() {
                 <div className="col-2" />
                 <div className="home-descriptions col-8">
                     <h1 className="page-title">Home</h1>
-                    <p>Are you a super-fan?  Want to know what the experts are saying during your 
-                    favorite team's game?  Team Tweets Live is here to keep you up to date with 
-                    live Tweets from the voices you want to hear.</p>
+                    <p>Check out random tweets from some fun accounts in the Tweet Gallery or search them out yourself on the Search page.</p>
                 </div>
             </div>
-            <div id="home-body" className="row">                
-                <div className="home-descriptions col-6">
-                    <h2>Live</h2>
-                    <p>See scores, stats, and live tweets about your favorite team as the game goes on</p>
-                    <img src="https://i.giphy.com/d8QcGOStfk8b1Q5lTJ.gif" alt="funny GIF" width="50%"></img>
-                </div>
-                <div className="home-descriptions col-6">
-                    <h2>Search</h2>
-                    <p>Search for accounts to add to your stream as well as remove accounts you're not
-                    interested in</p>
-                    <img src="https://i.giphy.com/d8QcGOStfk8b1Q5lTJ.gif" alt="funny GIF" width="50%"></img>
-                </div>
-            </div>
+            
         </div>
     )
 }
